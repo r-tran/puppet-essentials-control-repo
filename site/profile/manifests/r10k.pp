25d35bea7cf24814d5f0206ecbd9ac66c9ab1519
r-tran <r8tran@gmail.com>
2019-09-17 17:07:21 -0700

    Create r10k.pp

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,14 @@
+class profile::r10k {
+  class { 'r10k':
+    remote => 'https://github.com/r-tran/puppet-essentials-control-repo',
+  }
+  
+  class { 'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook':
+    user  => 'root',
+    group => 'root',
+  }
+}
